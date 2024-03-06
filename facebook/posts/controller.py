9eac40ac49492e9582e7b95987e7923211fff2be
Nguyễn Ngọc Đức <duco060303@gmail.com>
2024-03-06 21:02:41 +0700

    update post api: fix api create, create api update, delete

--- facebook/posts/controller.py
+++ facebook/posts/controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_jwt_extended import jwt_required, current_user
-from .services import get_new_feed_service, create_post_service
+from .services import get_new_feed_service, create_post_service, update_post_service, delete_post_service
 
 posts = Blueprint("posts", __name__)
 
@@ -15,3 +15,15 @@ def get_new_feed(page):
 @jwt_required()
 def create_post():
     return create_post_service(current_user)
+
+
+@posts.route("/post-management/post/update", methods=["PUT"])
+@jwt_required()
+def update_post():
+    return update_post_service(current_user)
+
+
+@posts.route("/post-management/post/delete/<int:id_post>", methods=["DELETE"])
+@jwt_required()
+def delete_post(id_post):
+    return delete_post_service(id_post, current_user)
--- facebook/posts/services.py
+++ facebook/posts/services.py
@@ -19,7 +19,12 @@ def get_new_feed_service(page, current_user):
 
 
 def create_post_service(current_user):
-    user_id = user_schema.dump(current_user)["id"]
+    try:
+        user_id = user_schema.dump(current_user)['id']
+    except Exception as e:
+        print(e)
+        return my_json(error_code=4, mess="token not match user id")
+
     data = json.loads(request.form['data'])
 
     check_data = data and ('title' in data) and ('status' in data)
@@ -55,5 +60,72 @@ def create_post_service(current_user):
             db.session.rollback()
             return my_json(error_code=1, mess="error in DB")
     else:
-        return my_json(error_code=2, mess="error validate data")
+        return my_json(error_code=3, mess="error validate data")
 
+
+def update_post_service(current_user):
+    try:
+        user_id = user_schema.dump(current_user)['id']
+    except Exception as e:
+        print(e)
+        return my_json(error_code=4, mess="token not match user id")
+
+    data = json.loads(request.form['data'])
+
+    check_data = data and ('title' in data) and ('status' in data) and ('id' in data)
+
+    if check_data and user_id:
+        id_post = data["id"]
+        title_new = data["title"]
+        status_new = data['status']
+        image_str_new = ""
+
+        data_image = request.files
+        if data_image:
+            file = data_image['image']
+            if file and allowed_file(file.filename):
+                try:
+                    filename_full = secure_filename(file.filename)
+                    filename = change_name_file(filename_full, user_id)
+                    file.save(get_path_upload(UPLOAD_POST_FOLDER, filename))
+                    image_str_new = filename
+                except Exception as e:
+                    print(e)
+                    return my_json(error_code=2, mess="error upload file")
+
+        try:
+            post = db.session.query(Posts).filter(Posts.id == id_post).first()
+            post.title = title_new
+            post.status = status_new
+            post.image = image_str_new
+
+            post_data = post_schema.dump(post)
+            db.session.commit()
+            return my_json(post_data)
+        except IndentationError:
+            db.session.rollback()
+            return my_json(error_code=1, mess="error in DB")
+    else:
+        return my_json(error_code=3, mess="error validate data")
+
+
+def delete_post_service(id_post, current_user):
+    try:
+        user_id = user_schema.dump(current_user)['id']
+    except Exception as e:
+        print(e)
+        return my_json(error_code=3, mess="token not match user id")
+
+    if id_post and user_id:
+        try:
+            post = db.session.query(Posts).filter(Posts.id == id_post).first()
+            post.isDelete = 1
+
+            post_data = post_schema.dump(post)
+            db.session.commit()
+            return my_json(post_data)
+        except IndentationError:
+            db.session.rollback()
+            return my_json(error_code=1, mess="error in DB")
+    else:
+        return my_json(error_code=2, mess="error validate data")
