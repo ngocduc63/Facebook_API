49b9edf22badf73fc0949be67c09f3f5ca8d79ef
Nguyễn Ngọc Đức <duco060303@gmail.com>
2024-05-07 21:13:56 +0700

    update file index

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from facebook import create_app
 from flask_cors import CORS
 from facebook.socketio_instance import socketio
+from flask import render_template
+
 
 if __name__ == "__main__":
     app = create_app()
     CORS(app, origins=['http://localhost:3000', 'https://example.com'], supports_credentials=True)
     socketio.init_app(app)
 
-    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True, log_output=True, use_reloader=False, debug=True)
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    socketio.run(app, allow_unsafe_werkzeug=True, debug=True)
