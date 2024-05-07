fcbd340a7c364fae0b54800e212fc6b384e2d370
Nguyễn Ngọc Đức <duco060303@gmail.com>
2024-05-07 20:55:08 +0700

    config cors

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ from facebook.socketio_instance import socketio
 
 if __name__ == "__main__":
     app = create_app()
-    CORS(app, origins='*', supports_credentials=True)
+    CORS(app, origins=['http://localhost:3000', 'https://example.com'], supports_credentials=True)
     socketio.init_app(app)
 
     socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True, log_output=True, use_reloader=False, debug=True)
-
-
-
