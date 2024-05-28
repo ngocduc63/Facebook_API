d8ce2ebe3a8ab77695d36bef76fec2b3d4c598e9
Nguyễn Ngọc Đức <duco060303@gmail.com>
2024-05-28 14:15:39 +0700

    Update config mongodb

--- facebook/config.py
+++ facebook/config.py
@@ -16,6 +16,7 @@ MESSAGE_FETCH_LIMIT = 20
 LIST_ROOM_CHAT_FETCH_LIMIT = 20
 ADD_FRIEND_FETCH_LIMIT = 10
 NOTIFICATION_FETCH_LIMIT = 10
-CONNECT_MONGO_DEV = 'mongodb+srv://test:123@facebook.7yqdc0f.mongodb.net/?retryWrites=true&w=majority&appName=facebook'
 CONNECT_MONGO_PRODUCT = \
+    'mongodb+srv://admin:0918273645abc@facebook.7yqdc0f.mongodb.net/?retryWrites=true&w=majority&appName=facebook'
+CONNECT_MONGO_DEV = \
     'mongodb+srv://ducdotb63:0918273645abc@facebook.hltsvun.mongodb.net/?retryWrites=true&w=majority&appName=Facebook'
